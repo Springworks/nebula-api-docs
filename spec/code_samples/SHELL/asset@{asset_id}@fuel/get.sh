2a26813ad8f7c84f9e8c4c0ab3ea312b86d0588d
Eddie <edipetres@gmail.com>
2019-12-05 17:16:06 +0100

    add new endpoints and refactor data into files

--- spec/code_samples/SHELL/asset@{asset_id}@fuel/get.sh
+++
@@ -1,4 +0,0 @@
-curl -X GET \
-  'https://nebula.service.autosense.ch/api/v1/asset/351622073876050/fuel' \
-  -H 'Authorization: Basic YXZyaW9zOkIzck4hNHZyMTBz'
-  
--- spec/code_samples/SHELL/asset@{asset_id}@fuel@history/get.sh
+++
@@ -1,4 +0,0 @@
-curl -X GET \
-  'https://nebula.service.autosense.ch/api/v1/asset/351622073876050/fuel/history?fromDate=2019-12-01T05:54:51Z&toDate=2019-12-02T05:54:51Z' \
-  -H 'Authorization: Basic YXZyaW9zOkIzck4hNHZyMTBz'
-  
